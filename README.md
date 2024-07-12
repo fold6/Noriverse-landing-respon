17e6600fa353552046691c914073c8d8ee864fdd
galaxy_star <kyejin0303@gmail.com>
2024-07-12 19:08:05 +0900

    Update

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Noriverse-landing-respon
- 스포츠 매니저 앱, '노리버스'를 소개하는 반응형 웹 페이지
+# Noriverse_homepage
+스포츠 매니저(각종 스포츠 기록,전적관리 및 매칭) 앱, 노리버스를 소개하는 반응형 웹 페이지 (디자인 및 퍼블리싱) <br>
+(Introducing Noribus, a responsive web page for a sports manager app that tracks<br>
+arious sports records, manages match histories, and facilitates matchmaking.)
+
+📌[바로가기](https://fold6.github.io/Noriverse_homepage/noriverse_home.html)
+
+<br>
+<br>
+
+## 기능 및 특징
+- 반응형 웹 페이지
+- 앱 장점 및 다운링크 표기 랜딩팝업 (24시간 보지않기가능)
+- 7가지 주요 서비스를 표기한 슬라이드
+- 종목별 소개 상세이미지
+- 6종의 경기매너 표기 서브페이지
---
+++ js/script.js
@@ -0,0 +1,167 @@
+$(document).ready(function () {
+
+
+  $(function () {
+    //햄버거 아이콘 클릭시 .mobile이 좌->우측 메뉴
+    $('.fa-bars').click(function () {
+      $('.mobile').addClass('bk');
+      $('.mobile').css({
+        'left': '0'
+      });
+      $('.fa-bars').hide();
+      $('.fa-times').show();
+    });
+
+    $('.fa-times').click(function () {
+      $('.bk').removeClass('bk');
+      $('.bk').hide(0);
+      $('.mobile').css({
+        'left': '-100%'
+      });
+      $('.fa-bars').show();
+      $('.fa-times').hide();
+      $('.bk').hide();
+    });
+
+  });
+
+  //헤더고정 스크롤
+  $(function () {
+    var headerTop = $('header').offset().top;
+    $(window).scroll(function () {
+      var scrollTop = $(window).scrollTop();
+      if (headerTop < scrollTop) {
+        $('header').addClass('fixed').stop();
+      } else {
+        $('header').removeClass('fixed');
+      }
+    });
+  })
+
+
+});
+
+//메인이미지 슬라이드
+$(function () {
+  $('.serv_wrap').slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 360,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+
+
+//24시간 팝업 
+$(function () {
+  // 쿠키 가져오기
+  var getCookie = function (cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1);
+      if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+    }
+    return "";
+  }
+
+  // 24시간 기준 쿠키 설정하기  
+  var setCookie = function (cname, cvalue, exdays) {
+    var todayDate = new Date();
+    todayDate.setTime(todayDate.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + todayDate.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+  }
+
+  var couponClose = function () {
+    if ($("input[name='chkbox']").is(":checked") == true) {
+      setCookie("close", "Y", 1);
+    }
+    $("#pop").fadeOut(0);
+  }
+
+  $(document).ready(function () {
+    var cookiedata = document.cookie;
+    console.log(cookiedata);
+    if (cookiedata.indexOf("close=Y") < 0) {
+      $("#pop").show();
+    } else {
+      $("#pop").fadeOut(200);
+    }
+    $(".btnClose").click(function () {
+      couponClose();
+      $("#sli").hide();
+      $(".pop").fadeOut(200);
+    });
+    $("#close_ok").click(function () {
+      couponClose();
+    });
+    $("#close_letsee").click(function () {
+      couponClose();
+      $("#popserv").show();
+    });
+
+
+    //다음버튼/이전버튼 이동
+    $("#next_1").click(function () {
+      couponClose();
+      $("#popserv").hide();
+      $('#popserv2').show();
+    });
+
+    $("#prev_2").click(function () {
+      couponClose();
+      $("#popserv2").hide();
+      $('#popserv').show();
+    });
+
+    $("#next_2").click(function () {
+      couponClose();
+      $("#popserv2").hide();
+      $('#popserv3').show();
+    });
+
+    $("#prev_3").click(function () {
+      couponClose();
+      $("#popserv3").hide();
+      $('#popserv2').show();
+    });
+
+    $("#next_3").click(function () {
+      couponClose();
+      $("#popserv3").hide();
+      $('#popserv4').show();
+    });
+
+
+  });
+
+
+});
